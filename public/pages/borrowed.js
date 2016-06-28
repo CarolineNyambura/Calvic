e380e5ccf54db10a35f561228f0b7eabe5c5e11f
Karole <karolinanyambura@gmail.com>
2016-06-28 22:41:05 +0300

    added new data two routes

--- public/pages/borrowed.js
+++ public/pages/borrowed.js
@@ -1,7 +1,128 @@
-var borrowed  = {
-  view:function(){
-    return m("div",[
-      m("div","borrowed"),
-    ])
-  }
+  var borrowed = [{
+      name:"final destination",
+      category:"horror",
+      dateBorrowed:"28-06-016",
+      dateOffReturn:"31-06-016"
+    },
+    {
+      name:"flying monkey",
+      category:"horror",
+      dateBorrowed:"28-06-016",
+      dateOffReturn:"31-06-016"
+    },
+    {
+      name:"jippers creepers",
+      category:"horror",
+      dateBorrowed:"28-06-016",
+      dateOffReturn:"31-06-016"
+    },
+    {
+      name:"mirror",
+      category:"horror",
+      dateBorrowed:"28-06-016",
+      dateOffReturn:"31-06-016"
+    },
+    {
+      name:"a thousand ways to die",
+      category:"horror",
+      dateBorrowed:"28-06-016",
+      dateOffReturn:"31-06-016"
+    },
+  ]
+  var videoInput = {
+    controller:function(){
+      return {
+        name:m.prop(""),
+        category:m.prop(""),
+        dateBorrowed:m.prop(""),
+        dateOffReturn:m.prop(""),
+      }
+    },
+  view:function(ctrl,args){
+    return m("form",{
+      onsubmit:function(e){
+        console.log("i have been selected")
+        borrowed.push({
+          name:ctrl.name(),
+          category:ctrl.category(),
+          dateBorrowed:ctrl.dateBorrowed(),
+          dateOffReturn:ctrl.dateOffReturn(),
+        }),
+        $('#modal2').closeModal();
+        e.preventDefault();
+      }
+    },[
+    input({
+      placeholder:"name",
+      oninput:m.withAttr("value",ctrl.name)
+    }),
+    m("input",{
+      placeholder:"category",
+      oninput:m.withAttr("value",ctrl.category)
+    }),
+    m("input",{
+      placeholder:"dateBorrowed",
+      oninput:m.withAttr("value",ctrl.dateBorrowed)
+    }),
+    m("input",{
+      placeholder:"dateOffReturn",
+      oninput:m.withAttr("value",ctrl.dateOffReturn)
+    })
+  ])
 }
+}
+  var videos  = {
+    controller:function(){
+      return {
+        videos:borrowed
+      }
+    },
+    view:function(ctrl, args){
+      console.log(ctrl.videos)
+      return m("div",{class:"container"},[
+        m(".row",[
+          m("br"),
+          m(".col l6",m("h4","the Videos Borrowed")),
+          m(".col l6",[
+            m("br"),
+            m("a",{
+              class:"btn right",
+              href:"#modal2",
+              onclick:function(){
+                $('#modal2').openModal();
+              },
+              config:function(){
+                $('.modal-trigger').leanModal();
+              }
+            },"Borrowed"),
+            modalMaker({
+              id:"modal2",
+              class:"modal",
+              body:inputForm
+            })
+          ])
+        ]),
+        m("table",[
+          m("thead",[
+            m("tr",[
+              m("th","name"),
+              m("th","category"),
+              m("th","dateBorrowed"),
+              m("th","dateOffReturn"),
+
+            ])
+          ]),
+          m("tbody",[
+            ctrl.videos.map(function(video){
+              return m("tr",[
+                m("td",video.name),
+                m("td",video.category),
+                m("td",video.dateBorrowed),
+                m("td",video.dateOffReturn)
+              ])
+            })
+          ])
+        ])
+      ])
+    }
+  }
--- public/pages/videos.js
+++ public/pages/videos.js
@@ -1,109 +1,119 @@
-var videosDb = [{
-    name:"camic sans"
-  },{
-    name:"camic red"
-  },{
-    name:"camic blue"
-  },{
-    name:"camic green"
-  }
-]
-
-function modalMaker(argument) {
-  return m("." + argument.class,{
-    id:argument.id
-  },[
-    m("div",[
-        m("p","this is an aweosme miuytoiodal"),
-        m(argument.body)
-    ])
-  ])
-}
-
-function input(argument){
-  return m("input",{
-          placeholder:argument.placeholder,
-          oninput: m.withAttr("value",argument.value),
-          value:argument.value()
-  })
-}
-
-var inputForm = {
-  controller:function(){
-    return {
-      name:m.prop("")
-    }
+  var videosDb = [{
+    name:"single ladies",
+    category:"thriller"
   },
+  {
+    name:"brave",
+    category:"cartoon"
+  },
+  {
+    name:"originals",
+    category:"general"
+    },
+    {
+      name:"vanish",
+      category:"horror"
+    },
+    {
+      name:"two broke girls",
+      category:"comedy"
+    },
+  ]
+
+  function modalMaker(argument) {
+    return m("." + argument.class,{
+      id:argument.id
+    },[
+      m("div",{class:"center"},[
+        m("p","Please input a new video name"),
+        m(argument.body)
+      ])
+    ])
+  }
+
+  function input(argument){
+    return m("input",{
+      placeholder:argument.placeholder,
+      oninput: m.withAttr("value",argument.value),
+      value:argument.value()
+    })
+  }
+
+  var inputForm = {
+    controller:function(){
+      return {
+        name:m.prop(""),
+        category:m.prop("")
+      }
+    },
   view:function (ctrl, args) {
     // body...
     return m("form",{
       onsubmit:function(e){
         console.log("i have been d=mhnbvlij")
         videosDb.push({
-          name:ctrl.name()
-        })
+          name:ctrl.name(),
+          category:ctrl.category()
+        }),
         $('#modal1').closeModal();
         e.preventDefault();
       }
     },[
       input({
-              placeholder:"name",
-              oninput: m.withAttr("value",ctrl.name),
-              value:ctrl.name
+        placeholder:"name",
+        oninput: m.withAttr("value",ctrl.name),
+        value:ctrl.name
       })
     ])
   }
 }
-
-var videos  = {
-  controller:function(){
-    return {
-      videos:videosDb
-    }
-  },
-  view:function(ctrl, args){
-    console.log(ctrl.videos)
-    return m("div",[
-      m(".row",[
+  var videos  = {
+    controller:function(){
+      return {
+        videos:videosDb
+      }
+    },
+    view:function(ctrl, args){
+      console.log(ctrl.videos)
+      return m("div",{class:"container"},[
+        m(".row",[
           m("br"),
-        m(".col l6",m("h4","Current videos")),
-        m(".col l6",[
-          m("br"),
-          m("a",{
-            class:"btn right",
-            href:"#modal1",
-            onclick:function(){
-              $('#modal1').openModal();
-            },
-            config:function(){
-              $('.modal-trigger').leanModal();
-            }
-          },"New Videos"),
-          modalMaker({
-            id:"modal1",
-            class:"modal",
-            body:inputForm
-          })
-        ])
-      ]),
-      m("table",[
-        m("thead",[
-          m("tr",[
-            m("th","name"),
-            m("th","name"),
-            m("th","name"),
-            m("th","name")
+          m(".col l6",m("h4","Video Catalogue")),
+          m(".col l6",[
+            m("br"),
+            m("a",{
+              class:"btn right",
+              href:"#modal1",
+              onclick:function(){
+                $('#modal1').openModal();
+              },
+              config:function(){
+                $('.modal-trigger').leanModal();
+              }
+            },"Save Video"),
+            modalMaker({
+              id:"modal1",
+              class:"modal",
+              body:inputForm
+            })
           ])
         ]),
-        m("tbody",[
-          ctrl.videos.map(function(video){
-            return m("tr",[
-              m("td",video.name)
+        m("table",[
+          m("thead",[
+            m("tr",[
+              m("th","Name"),
+              m("th","Category")
             ])
-          })
-
+          ]),
+          m("tbody",[
+            ctrl.videos.map(function(video){
+              return m("tr",[
+                m("td",video.name),
+                m("td",video.category)
+              ])
+            })
+          ])
         ])
       ])
-    ])
+    }
   }
-}
